9b84aebe56217ac2fe974ad19d6e569c2716aca9
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2023-07-16 18:04:32 +0500

    Creating Article class in models.py

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=70)
+    slug = models.SlugField(max_length=70, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    date_published = models.DateTimeField(default=timezone.now)
+    is_draft = models.BooleanField(default=True)
+    content = models.TextField()
