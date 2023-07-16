0cd7b501e1620386ba00f873e62a6f0c8fa075c8
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2023-07-16 16:09:42 +0500

    View functions' defintion.

--- myproject/views.py
+++ myproject/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 
 
+# Functions defined in views.py are called `view functions`
 # Creating a hello function which will display Hello World on webpage. 
 def hello(request):
     return HttpResponse('Hello World!')
